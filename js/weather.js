f1c253a834d9401fa090c342db8367d6c9a774d8
Rena Sakata <r.asanuma521@gmail.com>
2022-10-01 13:50:03 -0700

    add service worker

--- js/weather.js
+++ js/weather.js
@@ -1,115 +1,120 @@
-let lat, lon;
-let SettedLocation = "Current";
-var options = {
-    // If possible, set to true to read high-accuracy locations (e.g. GPS, etc.)
-    // However, this functionality affects battery life.
-    enableHighAccuracy: false, // Approximate value doesn't matter: default value
+// @ts-check
 
-    maximumAge: 30000,     // No modifications required until 5 minutes have passed : default value is 0
+(function () {
+    'use strict';
+    let lat, lon;
+    let SettedLocation = "Current";
+    var options = {
+        // If possible, set to true to read high-accuracy locations (e.g. GPS, etc.)
+        // However, this functionality affects battery life.
+        enableHighAccuracy: false, // Approximate value doesn't matter: default value
 
-    // How long will you wait to receive location information?
-    // The default is Infinity, so getCurrentPosition() waits indefinitely.
-    timeout: 15000    // don't wait more than 15 seconds.
-}
+        maximumAge: 30000,     // No modifications required until 5 minutes have passed : default value is 0
 
-if (navigator.geolocation) { // If you support geolocation, request the location. 
-    navigator.geolocation.getCurrentPosition(success, error, options);
-} else {
-    locationError.innerHTML = "Geolocation is not supported with this browser.";
-}
+        // How long will you wait to receive location information?
+        // The default is Infinity, so getCurrentPosition() waits indefinitely.
+        timeout: 15000    // don't wait more than 15 seconds.
+    }
 
-// If the geolocation request fails, call this function.
-function error(e) {
-    // The error object has numerical codes and text messages.
-    // The code values are as follows.
-    // 1: User does not provide permission to share location information.
-    // 2: Browser cannot get location.
-    // 3: Timeout occurred.
-    locationError.innerHTML = "Geolocation error" + e.code + ": " + e.message;
-}
+    if (navigator.geolocation) { // If you support geolocation, request the location. 
+        navigator.geolocation.getCurrentPosition(success, error, options);
+    } else {
+        locationError.innerHTML = "Geolocation is not supported with this browser.";
+    }
 
-// This function is called if the geolocation request is successful.
-function success(pos) {
+    // If the geolocation request fails, call this function.
+    function error(e) {
+        // The error object has numerical codes and text messages.
+        // The code values are as follows.
+        // 1: User does not provide permission to share location information.
+        // 2: Browser cannot get location.
+        // 3: Timeout occurred.
+        locationError.innerHTML = "Geolocation error" + e.code + ": " + e.message;
+    }
 
-    console.log(pos); // [Debugging] Check Position Object
+    // This function is called if the geolocation request is successful.
+    function success(pos) {
 
-    lat = latitude.innerHTML = pos.coords.latitude;
-    lon = longitude.innerHTML = pos.coords.longitude;
-}
+        console.log(pos); // [Debugging] Check Position Object
 
-// get api key from https://openweathermap.org/api
-const API_KEY = '030a06ef3a21f98e9fad039e0133fbbe';
-const IMAGE_BASE = "https://openweathermap.org/img/w/";
+        lat = latitude.innerHTML = pos.coords.latitude;
+        lon = longitude.innerHTML = pos.coords.longitude;
+    }
 
-getWeather.addEventListener('click', () => {
+    // get api key from https://openweathermap.org/api
+    const API_KEY = '030a06ef3a21f98e9fad039e0133fbbe';
+    const IMAGE_BASE = "https://openweathermap.org/img/w/";
 
-    // console.log("lat : " + lat + "lon : " + lon);
+    getWeather.addEventListener('click', () => {
 
-    fetch(
-        `https://pro.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`,
-     )
-        .then(response => response.json())
-        .then(data => {
-            locationWeather.innerHTML = " : " + SettedLocation;
-            temperature.innerHTML = data.main.temp;
-            weather.innerHTML = data.weather[0].main;
-            WeatherLatitude.innerHTML = lat;
-            WeatherLongitude.innerHTML = lon;
-        });
-});
+        // console.log("lat : " + lat + "lon : " + lon);
 
-citySearch.addEventListener('click', () => {
-    fetch(
-        `https://pro.openweathermap.org/geo/1.0/direct?q=${cityName.value}&appid=${API_KEY}`,
-    )
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            if(data.length) {
-                SettedLocation = data[0].name;
-                lat = data[0].lat;
-                lon = data[0].lon;
-                console.log(SettedLocation);
-                locationWeather.innerHTML = "";
-                locationForecast.innerHTML = "";
+        fetch(
+            `https://pro.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`,
+        )
+            .then(response => response.json())
+            .then(data => {
+                locationWeather.innerHTML = " : " + SettedLocation;
+                temperature.innerHTML = data.main.temp;
+                weather.innerHTML = data.weather[0].main;
+                WeatherLatitude.innerHTML = lat;
+                WeatherLongitude.innerHTML = lon;
+            });
+    });
+
+    citySearch.addEventListener('click', () => {
+        fetch(
+            `https://pro.openweathermap.org/geo/1.0/direct?q=${cityName.value}&appid=${API_KEY}`,
+        )
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                if (data.length) {
+                    SettedLocation = data[0].name;
+                    lat = data[0].lat;
+                    lon = data[0].lon;
+                    console.log(SettedLocation);
+                    locationWeather.innerHTML = "";
+                    locationForecast.innerHTML = "";
+                    thirtyDaysWeatherTable.innerHTML = "";
+                } else {
+                    console.log("No city");
+                }
+            });
+    })
+
+    forecast.addEventListener('click', () => {
+        fetch(
+            `https://pro.openweathermap.org/data/2.5/forecast/climate?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`,
+        )
+            .then(response => response.json())
+            .then(data => {
+                // whole data is displayed console.log, instead access below link to show whole data
+                // https://api.openweathermap.org/data/2.5/forecast/climate?lat=49.2288149&lon=-123.1141329&appid=030a06ef3a21f98e9fad039e0133fbbe
+                // console.log(JSON.stringify(data));
+                let list = data.list;
+                locationForecast.innerHTML = " : " + SettedLocation;
                 thirtyDaysWeatherTable.innerHTML = "";
-            } else {
-                console.log("No city");
-            }
-        });
-})
+                for (item of list) {
+                    // console.log("forecast weather date : " + item.dt_txt);
+                    // console.log("forecast weather data : " + item.weather[0].main);
+                    let symbol = item.weather[0].icon + ".png";
+                    let imageSrcUrl = IMAGE_BASE + symbol;
+                    let row = thirtyDaysWeatherTable.insertRow();
 
-forecast.addEventListener('click', () => {
-    fetch(
-        `https://pro.openweathermap.org/data/2.5/forecast/climate?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`,
-     )
-        .then(response => response.json())
-        .then(data => {
-            // whole data is displayed console.log, instead access below link to show whole data
-            // https://api.openweathermap.org/data/2.5/forecast/climate?lat=49.2288149&lon=-123.1141329&appid=030a06ef3a21f98e9fad039e0133fbbe
-            // console.log(JSON.stringify(data));
-            let list = data.list;
-            locationForecast.innerHTML = " : " + SettedLocation;
-            thirtyDaysWeatherTable.innerHTML = "";
-            for (item of list) {
-                // console.log("forecast weather date : " + item.dt_txt);
-                // console.log("forecast weather data : " + item.weather[0].main);
-                let symbol = item.weather[0].icon + ".png";
-                let imageSrcUrl = IMAGE_BASE + symbol;
-                let row = thirtyDaysWeatherTable.insertRow();
+                    // define cells each rows.
+                    const cellDate = row.insertCell(0);
+                    const cellWeatherIcon = row.insertCell(1);
+                    const cellWeather = row.insertCell(2);
+                    const cellTempMin = row.insertCell(3);
+                    const cellTempMax = row.insertCell(4);
 
-                // define cells each rows.
-                const cellDate = row.insertCell(0);
-                const cellWeatherIcon = row.insertCell(1);
-                const cellWeather = row.insertCell(2);
-                const cellTempMin = row.insertCell(3);
-                const cellTempMax = row.insertCell(4);
-
-                cellDate.innerHTML = new Date(parseInt(item.dt + "000")).toDateString();
-                cellWeatherIcon.innerHTML = `<img src=${imageSrcUrl}>`;
-                cellWeather.innerHTML = item.weather[0].main;
-                cellTempMin.innerHTML = item.temp.min + "℃ |";
-                cellTempMax.innerHTML = item.temp.max + "℃";
-            }
-        });
-});
+                    cellDate.innerHTML = new Date(parseInt(item.dt + "000")).toDateString();
+                    cellWeatherIcon.innerHTML = `<img src=${imageSrcUrl}>`;
+                    cellWeather.innerHTML = item.weather[0].main;
+                    cellTempMin.innerHTML = item.temp.min + "℃ |";
+                    cellTempMax.innerHTML = item.temp.max + "℃";
+                }
+            });
+    });
+})()
